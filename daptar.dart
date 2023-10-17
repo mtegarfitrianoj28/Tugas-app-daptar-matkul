f1d6e6b7e09d518db7902ba8a64572730b3df4a2
Muhamad Tegar Fitriano Jagat <89504891+mtegarfitrianoj28@users.noreply.github.com>
2023-10-16 22:32:26 -0700

    Update daptar.dart

--- daptar.dart
+++ daptar.dart
@@ -1,14 +1,1 @@
-class Course {
-  final String name;
-  final int sks;
-  final String image;
 
-  Course({required this.name, required this.sks, required this.image});
-}
-
-List<Course> courses = [
-  Course(name: 'Animasi Komputer', sks: 3, image: 'assets/images/anikom.png'),
-  Course(name: 'Data mining', sks: 3, image: 'assets/images/datmin.png'),
-  Course(name: 'Mobile computing', sks: 3, image: 'assets/images/mocom.png'),
-  // Tambahkan data mata kuliah lainnya di sini
-];
